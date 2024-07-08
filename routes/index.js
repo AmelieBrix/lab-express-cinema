5f9da64082f82824148ca52b1dcf44ee8ac88f04
Amelie Brix <brix.amelie@googlemail.com>
2024-07-08 22:00:53 +0200

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('./models/Movie.model.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => 
+    {
+        Movie.find()
+        .then(data => {
+            res.render('movies', {movies: data})
+        })
+    }
+    
+);
+
+router.get('/movies/:id', (req, res, next) => 
+    {
+        Movie.findById(req.params.id)
+        .then(data => {
+            res.render('details', {details: data})
+        })
+    }
+    
+);
 
 module.exports = router;
